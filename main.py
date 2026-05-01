03a7cbd29c4267776fdea6049bd67f4f1d3b72d5
aravindnair895 <101332829+aravindnair895@users.noreply.github.com>
2026-05-01 08:20:51 +0530

    added agent code

---
+++ main.py
@@ -0,0 +1,99 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+# from langchain_openai import ChatOpenAI
+# from langchain_anthropic import ChatAnthropic
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain_classic.agents import AgentExecutor, create_tool_calling_agent
+from tools import duckduckgo_search, get_price_data, calculated_rsi 
+import re
+# from tools import search_tool
+
+
+load_dotenv()
+
+# class Responsemessage(BaseModel):
+#     topic: str
+#     summary: str
+#     sources: list[str]
+#     tools_used: list[str]
+
+class Tradesignal(BaseModel):
+    pair: str
+    bias: str
+    entry: float
+    stop_loss: float
+    take_profit: float
+    risk_reward: float
+    confidance: float
+
+
+# llm = ChatAnthropic(model="claude-3-5-sonnet-20241022")
+# llm2 = ChatOpenAI()
+llm3 = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+parser = PydanticOutputParser(pydantic_object=Tradesignal)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", """You are a forex trading assistant.
+
+    Use tools to analyze market data.
+
+    Rules:
+    - Always provide structured output
+    - Include entry, stop_loss, take_profit
+    - Risk reward must be >= 1.5
+    - If unsure, return "NO TRADE"
+      \n{format_instructions}""",),
+    ("placeholder","{chat_history}"),
+    ("human","{query}"),
+    ("placeholder","{agent_scratchpad}"),]).partial(format_instructions=parser.get_format_instructions())
+
+tools = [duckduckgo_search, get_price_data, calculated_rsi]
+agent = create_tool_calling_agent(
+    llm=llm3, 
+    tools=tools, 
+    prompt=prompt
+    )
+
+agent_executor = AgentExecutor(
+    agent=agent, 
+    tools=tools, 
+    verbose=True
+    )
+
+query = input("What is on your mind? ")
+raw_response = agent_executor.invoke({"query": query})
+# print(raw_response)
+
+try:
+    output_chunks = raw_response.get("output")
+    if isinstance(output_chunks, list):
+        full_text = ""
+        for chunk in output_chunks:
+            if isinstance(chunk, dict) and "text" in chunk:
+                full_text += chunk["text"]
+            elif isinstance(chunk, str):
+                full_text += chunk
+    elif isinstance(output_chunks, str):
+        full_text = output_chunks
+    else:
+        raise ValueError("Unexpected output format")
+    
+    cleaned = re.sub(r"```json|```","", full_text).strip()
+    start = cleaned.find("{")
+    end = cleaned.rfind("}") + 1
+    json_text = cleaned[start:end]
+
+    print("Extracted JSON:", json_text)
+
+    structured_response = parser.parse(json_text)
+    # structured_response = parser.parse(raw_response.get("output")[0]["text"])
+    print(structured_response)
+
+except Exception as e:
+    print("Failed to parse response:", e)
+    
+# response = llm3.invoke("what is the meaning of life?")
+# print(response.content)
---
+++ tools.py
@@ -0,0 +1,39 @@
+from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun
+from langchain_community.utilities import WikipediaAPIWrapper
+from langchain.tools import tool
+from datetime import datetime
+import yfinance as yf
+import pandas as pd
+
+search = DuckDuckGoSearchRun()
+
+@tool
+def duckduckgo_search(query: str) -> str:
+    """Searches the web for information"""
+    return search.run(query)
+
+# search_tool = tool(
+#     name="duckduckgo_search",
+#     description="Searches the web for information",
+#     func=search.run
+# )
+
+@tool
+def get_price_data(symbol: str) -> str:
+    """Get latest price data of a forex pair like EURUSD=X"""
+    data = yf.download(symbol, period="1d", interval="5m")
+    return data.tail(1).to_string()
+
+@tool
+def calculated_rsi(symbol: str) -> str:
+    """Calculate the Relative Strength Index (RSI) for a forex pair"""
+    data = yf.download(symbol, period="5d", interval="5m")
+    delta = data['Close'].diff()
+    gain = (delta.where(delta > 0, 0)).rolling(window=14).mean()
+    loss = (-delta.where(delta < 0, 0)).rolling(window=14).mean()
+    rs = gain / loss
+    rsi = 100 - (100 / (1 + rs))
+    latest_rsi = rsi.iloc[-1]
+    if hasattr(latest_rsi, 'values'):
+        latest_rsi = latest_rsi.values[0]  # Convert to native Python float if it's a numpy type
+    return f"The latest RSI for {symbol} is {latest_rsi:.2f}"
